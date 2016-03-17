d62e63135f645577ac1273417e8e77e6ae820bc6
HongQuanli <hongquanli@qq.com>
2016-03-17 18:00:23 +0800

    //

---
+++ net.cloudkit.ecosystem/src/main/java/net/cloudkit/ecosystem/application/CloudKitEcosystemApplication.java
@@ -0,0 +1,12 @@
+package net.cloudkit.ecosystem.application;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CloudKitEcosystemApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CloudKitEcosystemApplication.class, args);
+	}
+}
